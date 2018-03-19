83f9fb632f0d19c1327785cdb079a8c161f80989
George K <gkh36@uclive.ac.nz>
2018-03-19 22:39:05 +1300

    Indentation and some more comment fixing

--- milestrone_one/ADCdemo1.c
+++ milestrone_one/ADCdemo1.c
@@ -30,20 +30,18 @@
 //*****************************************************************************
 // Global variables
 //*****************************************************************************
-static circBuf_t g_inBuffer;		// Buffer of size BUF_SIZE integers (sample values)
+static circBuf_t g_inBuffer;    // Buffer of size BUF_SIZE integers (sample values)
 static uint32_t g_ulSampCnt;	// Counter for the interrupts
 
-								//*****************************************************************************
-								//
-								// The interrupt handler for the for SysTick interrupt.
-								//
-								//*****************************************************************************
+//*****************************************************************************
+//
+// The interrupt handler for the for SysTick interrupt.
+//
+//*****************************************************************************
 void
 SysTickIntHandler(void)
 {
-	//
 	// Initiate a conversion
-	//
 	ADCProcessorTrigger(ADC0_BASE, 3);
 	g_ulSampCnt++;
 
@@ -62,14 +60,13 @@ ADCIntHandler(void)
 {
 	uint32_t ulValue;
 
-	//
 	// Get the single sample from ADC0.  ADC_BASE is defined in
 	// inc/hw_memmap.h
 	ADCSequenceDataGet(ADC0_BASE, 3, &ulValue);
-	//
+	
 	// Place it in the circular buffer (advancing write index)
 	writeCircBuf(&g_inBuffer, ulValue);
-	//
+	
 	// Clean up, clearing the interrupt
 	ADCIntClear(ADC0_BASE, 3);
 }
@@ -83,14 +80,14 @@ initClock(void)
 	// Set the clock rate to 20 MHz
 	SysCtlClockSet(SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
 		SYSCTL_XTAL_16MHZ);
-	//
+	
 	// Set up the period for the SysTick timer.  The SysTick timer period is
 	// set as a function of the system clock.
 	SysTickPeriodSet(SysCtlClockGet() / SAMPLE_RATE_HZ);
-	//
+	
 	// Register the interrupt handler
 	SysTickIntRegister(SysTickIntHandler);
-	//
+	
 	// Enable interrupt and device
 	SysTickIntEnable();
 	SysTickEnable();
@@ -99,7 +96,7 @@ initClock(void)
 void
 initADC(void)
 {
-	//
+	
 	// The ADC0 peripheral must be enabled for configuration and use.
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
 
@@ -108,27 +105,16 @@ initADC(void)
 	// conversion.
 	ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
 
-	//
-	// Configure step 0 on sequence 3.  Sample channel 0 (ADC_CTL_CH0) in
-	// single-ended mode (default) and configure the interrupt flag
-	// (ADC_CTL_IE) to be set when the sample is done.  Tell the ADC logic
-	// that this is the last conversion on sequence 3 (ADC_CTL_END).  Sequence
-	// 3 has only one programmable step.  Sequence 1 and 2 have 4 steps, and
-	// sequence 0 has 8 programmable steps.  Since we are only doing a single
-	// conversion using sequence 3 we will only configure step 0.  For more
-	// on the ADC sequences and steps, refer to the LM3S1968 datasheet.
-	ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH9 | ADC_CTL_IE |
-		ADC_CTL_END);
-
-	//
+	
+	// Configure step 0 on sequence 3
+	ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH9 | ADC_CTL_IE | ADC_CTL_END);
+	
 	// Since sample sequence 3 is now configured, it must be enabled.
 	ADCSequenceEnable(ADC0_BASE, 3);
 
-	//
 	// Register the interrupt handler
 	ADCIntRegister(ADC0_BASE, 3, ADCIntHandler);
-
-	//
+	
 	// Enable interrupts for ADC0 sequence 3 (clears any outstanding interrupts)
 	ADCIntEnable(ADC0_BASE, 3);
 }
@@ -150,7 +136,7 @@ displayMeanVal(uint16_t meanVal, char* units, uint32_t count)
 {
 	char string[17];  // 16 characters across the display
 	OLEDStringDraw("                ", 0, 1); // Clear the display
-											  // Form a new string for the line.
+	// Form a new string for the line.
 	usnprintf(string, sizeof(string), "Alt = %d%s", meanVal, units);
 	// Update line on display.
 	OLEDStringDraw(string, 0, 1);
@@ -194,7 +180,7 @@ main(void)
 			baseHeight = currentHeight;
 		}
 		else {
-			// Calculate helicopter altitude as a percentage
+			// Calculate helicopter altitude as a rounded percentage
 			percentage = ((2 * (baseHeight - currentHeight) / 8) + 1) / 2;
 			percentage = (currentHeight > baseHeight) ? 0 : percentage;
 		}
