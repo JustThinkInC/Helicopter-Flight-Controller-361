83a05855b517bb81ecc8384df37f5db1f8aa5b40
Liam Laing <ljl48@uclive.ac.nz>
2018-03-23 11:23:13 +1300

    added a fully initalized project that can be used

---
+++ empty_fully_init_project/Debug/Milestone1.d
@@ -0,0 +1,43 @@
+# FIXED
+
+Milestone1.obj: ../Milestone1.c
+Milestone1.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+Milestone1.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
+Milestone1.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+Milestone1.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+Milestone1.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h
+Milestone1.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pwm.h
+Milestone1.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+Milestone1.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+Milestone1.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h
+Milestone1.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+Milestone1.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+Milestone1.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h
+Milestone1.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h
+Milestone1.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h
+Milestone1.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h
+Milestone1.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h
+Milestone1.obj: ../circBufT.h
+Milestone1.obj: P:/lab_a02_group_10/OrbitOLED/OrbitOLEDInterface.h
+Milestone1.obj: ../buttons4.h
+
+../Milestone1.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pwm.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h: 
+../circBufT.h: 
+P:/lab_a02_group_10/OrbitOLED/OrbitOLEDInterface.h: 
+../buttons4.h: 
